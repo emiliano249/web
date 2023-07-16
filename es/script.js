0dd083f4c916074359fca9148e99c51f7e85e521
Emiliano Esteban <50309405+emiliano249@users.noreply.github.com>
2023-07-16 19:29:11 -0300

    Add files via upload

--- es/script.js
+++ es/script.js
@@ -36,14 +36,14 @@ $(document).ready(function(){
 
   // typing text animation script
   var typed = new Typed(".typing", {
-      strings: ["Diseñador gráfico y visual","Desarrollador WEB","Animador 2D","Freelancer"],
+      strings: ["Diseñador gráfico y visual","Diseñador y desarrollador WEB","Animador 2D","Freelancer"],
       typeSpeed: 100,
       backSpeed: 60,
       loop: true
   });
 
   var typed = new Typed(".typing-2", {
-      strings: ["Diseñador gráfico y visual","Desarrollador WEB","Animador 2D","Freelancer"],
+      strings: ["Diseñador gráfico y visual","Diseñador y desarrollador WEB","Animador 2D","Freelancer"],
       typeSpeed: 100,
       backSpeed: 60,
       loop: true
